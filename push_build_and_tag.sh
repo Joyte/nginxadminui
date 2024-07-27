6178763de87627b80fd35a1c1c0e2c77c368b2ed
Eleanor Prins <eleanor@prins.life>
2024-07-28 00:23:13 +1200

    Update push_build_and_tag.sh

--- push_build_and_tag.sh
+++ push_build_and_tag.sh
@@ -2,3 +2,15 @@
 
 # Push to github
 git push
+
+# Get the latest tag
+latest_tag=$(git log -n1 --format="%h")
+
+# Docker build with :latest and :$latest_tag
+docker build -t joyte/nginxadminui:latest -t joyte/nginxadminui:$latest_tag .
+
+# Docker push both tags
+docker push joyte/nginxadminui:latest
+docker push joyte/nginxadminui:$latest_tag
+
+# Done
